f8705e1b5fe18c08a7deecce46e2ee26d02f991f
sufyan4081 <srasheed4081@gmail.com>
2024-11-05 22:34:32 +0530

    chagne http to https

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://api.smarthought.in/api/",
+  baseURL: "https://api.smarthought.in/api/",
   headers: {
     "Content-Type": "application/json",
   },
